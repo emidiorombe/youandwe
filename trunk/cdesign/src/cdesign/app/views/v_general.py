39f6b32e95640b23fe9658632d4ca993c9c9064e
rafael.nunes <rafael.nunes@396a046e-fee2-11de-8589-c9554b7fe0d5>
2011-01-06 19:21:54 +0000

    useless

--- trunk/cdesign/src/cdesign/app/views/v_general.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from django.utils.translation import ugettext as msg
-
-def index(request):
-    myvar = msg('ola_enfermeira')
-    return render_to_response('index.html', locals())
-
-def erro404(request):
-    return render_to_response('404.html')
