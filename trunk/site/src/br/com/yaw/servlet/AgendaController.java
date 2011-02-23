2d53135b304769d642d29ee8543e327a336ec7e0
rafael.nunes@gmail.com <rafael.nunes@gmail.com@396a046e-fee2-11de-8589-c9554b7fe0d5>
2011-02-23 04:06:40 +0000

    coloquei o jquery no site, e implementei a busca da agende de turma em uma servlet com json

---
+++ trunk/site/src/br/com/yaw/servlet/AgendaController.java
@@ -0,0 +1,29 @@
+package br.com.yaw.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgendaController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("application/json");
+		PrintWriter out = resp.getWriter();
+		StringBuilder sb = new StringBuilder("[ ");
+		sb.append("{ \"id\": \"AJABCT05\", \"desc\":\"Academia Java\", \"dt\":\"22&#47;Fev&#47;2011\", \"per\":\"Ter&#231;as e Quintas, das 19 as 23h\", \"link\":\"/treinamentos/academiaJava.html\" }").append(" , ")
+			.append("{ \"id\": \"AJABCT06\", \"desc\":\"Academia Java\", \"dt\":\"02&#47;Abr&#47;2011\", \"per\":\"S&#225;bados, das 10 as 19h\", \"link\":\"/treinamentos/academiaJava.html\" }").append(" , ")
+			.append("{ \"id\": \"AWABCT02\", \"desc\":\"Academia Web\", \"dt\":\"09&#47;Mai&#47;2011\", \"per\":\"Segundas e Quartas, das 19 as 23h\", \"link\":\"/treinamentos/academiaWeb.html\" }").append(" , ")
+			.append("{ \"id\": \"APABCT01\", \"desc\":\"Academia do Programador\", \"dt\":\"14&#47;Mai&#47;2011\", \"per\":\"S&#225;bados, das 10 as 19h\", \"link\":\"/treinamentos/academiaProgramador.html\" }");
+		
+		sb.append(" ]");
+		out.println(sb.toString());
+		out.close();
+	}
+	
+}
