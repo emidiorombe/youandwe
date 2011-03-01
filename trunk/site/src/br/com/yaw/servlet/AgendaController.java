5bef29b9105292ed8cea25f5c3c54c213a7d3a51
rafael.nunes@gmail.com <rafael.nunes@gmail.com@396a046e-fee2-11de-8589-c9554b7fe0d5>
2011-03-01 11:09:00 +0000

    atualizacao agenda

--- trunk/site/src/br/com/yaw/servlet/AgendaController.java
+++ trunk/site/src/br/com/yaw/servlet/AgendaController.java
@@ -16,8 +16,7 @@ public class AgendaController extends HttpServlet {
 		resp.setContentType("application/json");
 		PrintWriter out = resp.getWriter();
 		StringBuilder sb = new StringBuilder("[ ");
-		sb.append("{ \"id\": \"AJABCT05\", \"desc\":\"Academia Java\", \"dt\":\"22&#47;Fev&#47;2011\", \"per\":\"Ter&#231;as e Quintas, das 19 as 23h\", \"link\":\"/treinamentos/academiaJava.html\" }").append(" , ")
-			.append("{ \"id\": \"AJABCT06\", \"desc\":\"Academia Java\", \"dt\":\"02&#47;Abr&#47;2011\", \"per\":\"S&#225;bados, das 10 as 19h\", \"link\":\"/treinamentos/academiaJava.html\" }").append(" , ")
+		sb.append("{ \"id\": \"AJABCT06\", \"desc\":\"Academia Java\", \"dt\":\"02&#47;Abr&#47;2011\", \"per\":\"S&#225;bados, das 10 as 19h\", \"link\":\"/treinamentos/academiaJava.html\" }").append(" , ")
 			.append("{ \"id\": \"AWABCT02\", \"desc\":\"Academia Web\", \"dt\":\"09&#47;Mai&#47;2011\", \"per\":\"Segundas e Quartas, das 19 as 23h\", \"link\":\"/treinamentos/academiaWeb.html\" }").append(" , ")
 			.append("{ \"id\": \"APABCT01\", \"desc\":\"Academia do Programador\", \"dt\":\"14&#47;Mai&#47;2011\", \"per\":\"S&#225;bados, das 10 as 19h\", \"link\":\"/treinamentos/academiaProgramador.html\" }");
 		
